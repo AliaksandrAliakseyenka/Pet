e12416ce4fccfbbdadb475522afdb95366b76062
AliaksandrAliakseyenka <ali.aliaksandr@gmail.com>
2023-01-23 19:23:42 +0300

    authdemov3

--- authdemo/server.py
+++ authdemo/server.py
@@ -3,6 +3,19 @@ from fastapi.responses import Response
 
 app = FastAPI()
 
+users = {
+	"alex@gmail.com": {
+		"name": "alex",
+		"password": "qwerty",
+		"balance": 10_000
+	},
+	"dmitry@gmail.com": {
+		"name": "dima",
+		"password": "123456",
+		"balance": 17_000
+	}
+}
+
 
 # uvicorn server:app --reload
 
@@ -15,4 +28,12 @@ def index_page():
 
 @app.post("/login")
 def login_page(user_name: str = Form(...), password: str = Form(...)):
-	return Response(f"Ваш Login: {user_name}, password: {password}", media_type="text/html")
+	user = users.get(user_name)
+	if not user or user["password"] != password:
+		return Response("Not found user", media_type="text/html")
+
+	response = Response(
+		f"Hi user, {user['name']}!, Balance: {user['balance']}", media_type="text/html"
+	)
+	response.set_cookie(key="username", value=user_name)
+	return response
