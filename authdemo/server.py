ccc9b004a1e29fef848dbbc7a9e9c73a48db5830
AliaksandrAliakseyenka <ali.aliaksandr@gmail.com>
2023-01-23 17:50:50 +0300

    authdemov2

--- authdemo/server.py
+++ authdemo/server.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import Response
 
 app = FastAPI()
 
-#uvicorn server:app --reload
+
+# uvicorn server:app --reload
+
 @app.get("/")
 def index_page():
-	return Response("Hi user", media_type="text/html")
+	with open(r'/Users/alexalex/Desktop/practice_python/Pet/authdemo/sign-in-form/index.html', 'r') as file:
+		login_page = file.read()
+	return Response(login_page, media_type="text/html")
+
+
+@app.post("/login")
+def login_page(user_name: str = Form(...), password: str = Form(...)):
+	return Response(f"Ваш Login: {user_name}, password: {password}", media_type="text/html")
